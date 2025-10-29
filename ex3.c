f683856fa59d41f7ff3452b92cf0ac2b43114c88
ridvan20 <ridvanurhan2005@icloud.com>
2025-10-29 23:26:45 +0300

    Animal Exercise
    Rıdvan Urhan

--- ex3.c
+++ ex3.c
@@ -1,2 +1,20 @@
+//Rıdvan Urhan
+#include <stdio.h>
+#include <string.h>
+int main() {
+	char a;
+	printf("Do you pet a cat or a dog? \n");
+	printf("Pres 'C or c' for cat or 'D or d' for dog : ");
+	scanf("%c" , &a);
+	if (a == 'c' || a == 'C') {
+			printf("Miaw Miaw \n");
+	}
+	else if(a == 'd' || a == 'D') {
+		printf("Hav hav \n");
+	}
+	else { 
+	printf("You did not use any of character. Use correct characters\n");
+	}
+} 
 
 
