1881705e75741985b6845ef009ba069e3682930e
ridvan20 <ridvanurhan2005@icloud.com>
2025-10-29 23:10:12 +0300

    Guess Number Exercise
    Rıdvan Urhan

--- ex1.c
+++ ex1.c
@@ -1,1 +1,16 @@
-
+//Rıdvan Urhan
+#include <stdio.h>
+int main() {
+	int theNumber = 2;
+	int guess;
+	printf("Try to guess the true number between 1-5\n");
+	do{
+		printf("Choose a number: ");
+		scanf("%d" , &guess);
+		if (guess != theNumber) {
+			printf("Nice try, but it's wrong. Try again: ");
+		}
+	} while (guess != theNumber);
+	printf("Perceft. You found the correct number.\n");
+	return 0;
+}
