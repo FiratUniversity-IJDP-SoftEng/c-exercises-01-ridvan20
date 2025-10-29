2e3b9f179aa24b83cbadcf6ead9145fcd2080bf5
ridvan20 <ridvanurhan2005@icloud.com>
2025-10-29 23:40:26 +0300

    Hi Exercise
    Rıdvan Urhan

--- ex5.c
+++ ex5.c
@@ -1,1 +1,11 @@
-
+//Rıdvan Urhan
+#include <stdio.h>
+void sayHi(const char* name) {
+	printf("Hi, %s!\n" , name);
+}
+int main() {
+	char names[50];
+	printf("Can you enter your name plaese: ");
+	scanf("%49s" , names);
+	sayHi(names);
+}
